1ac2695c7b8972af6095a5a86fdb58542af24b52
makaota <74915165+makaota@users.noreply.github.com>
2022-06-02 19:15:14 +0200

    Basic Modifiers

--- app/src/main/java/android/app/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/android/app/learningjetpackcompose/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.app.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
@@ -21,17 +23,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
 
-            Row(modifier = Modifier
-                .width(200.dp)
-                .fillMaxHeight(0.4f)
-                .background(Color.Green),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            )
-            {
-                Text("Hello!")
-                Text( "Jetpack")
-                Text( "Compose")
+            Column(modifier = Modifier
+                .background(Color.Green)
+                .fillMaxHeight(0.5f)
+                .fillMaxWidth()
+                //   .padding(10.dp)
+                .border(5.dp, Color.Magenta)
+                .padding(5.dp)
+                .border(5.dp, Color.Blue)
+                .padding(5.dp)
+                .border(10.dp, Color.Red)
+                .padding(10.dp)
+  //              .requiredWidth(600.dp)
+                ) {
+//                Text("Hello", modifier = Modifier
+//                    .border(5.dp, Color.Yellow)
+//                    .padding(5.dp)
+//                    .offset(20.dp, 20.dp)
+//                    .border(10.dp, Color.Black)
+//                    .padding(10.dp)
+//                )
+                Text("Hello", modifier = Modifier.clickable {
+
+                } )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text("World")
             }
         }
     }
