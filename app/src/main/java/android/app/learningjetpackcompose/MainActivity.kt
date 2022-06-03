c64de439c38318c8304bb5eb682466e5fbfd13f6
makaota <74915165+makaota@users.noreply.github.com>
2022-06-03 14:21:25 +0200

    Image card composable

--- app/src/main/java/android/app/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/android/app/learningjetpackcompose/MainActivity.kt
@@ -10,45 +10,79 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import android.app.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            Column(modifier = Modifier
-                .background(Color.Green)
-                .fillMaxHeight(0.5f)
-                .fillMaxWidth()
-                //   .padding(10.dp)
-                .border(5.dp, Color.Magenta)
-                .padding(5.dp)
-                .border(5.dp, Color.Blue)
-                .padding(5.dp)
-                .border(10.dp, Color.Red)
-                .padding(10.dp)
-  //              .requiredWidth(600.dp)
-                ) {
-//                Text("Hello", modifier = Modifier
-//                    .border(5.dp, Color.Yellow)
-//                    .padding(5.dp)
-//                    .offset(20.dp, 20.dp)
-//                    .border(10.dp, Color.Black)
-//                    .padding(10.dp)
-//                )
-                Text("Hello", modifier = Modifier.clickable {
+            val painter = painterResource(id = R.drawable.kermit)
+            val contentDescription = "kermit is drinking coffee"
+            val title = "kermit is drinking coffee"
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)){
+                ImageCard(painter = painter,
+                    contentDescription = contentDescription,
+                    title = title)
+            }
 
-                } )
-                Spacer(modifier = Modifier.height(50.dp))
-                Text("World")
+
+        }
+    }
+}
+
+@Composable
+fun ImageCard(painter: Painter,
+              contentDescription: String,
+              title: String,
+              modifier: Modifier = Modifier){
+
+    Card(modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+
+    ) {
+        Box(modifier = Modifier.height(200.dp)){
+
+           // val image : Painter = painterResource(id = R.drawable.kermit)
+
+            Image( painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(Brush.verticalGradient(
+                    colors = listOf(
+                        Color.Transparent,
+                        Color.Black
+                    ), startY = 300f)
+                ))
+
+            Box(modifier = Modifier
+                .fillMaxHeight()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart){
+                Text(title, style = TextStyle(Color.White, fontSize = 16.sp))
             }
         }
     }
+
 }
