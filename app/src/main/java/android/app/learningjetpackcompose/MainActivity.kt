5ef1a7e889de0c36b60948984378d35bd45e5f91
makaota <74915165+makaota@users.noreply.github.com>
2022-06-03 16:22:54 +0200

    Text Styling

--- app/src/main/java/android/app/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/android/app/learningjetpackcompose/MainActivity.kt
@@ -23,66 +23,75 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val fontFamily = FontFamily(
+            Font(R.font.roboto_thin, FontWeight.Thin),
+            Font(R.font.roboto_black, FontWeight.Black),
+            Font(R.font.roboto_bold, FontWeight.Bold),
+            Font(R.font.roboto_medium, FontWeight.Medium),
+            Font(R.font.roboto_regular, FontWeight.Normal),
+
+
+            )
+
         setContent {
 
-            val painter = painterResource(id = R.drawable.kermit)
-            val contentDescription = "kermit is drinking coffee"
-            val title = "kermit is drinking coffee"
-            Box(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(16.dp)){
-                ImageCard(painter = painter,
-                    contentDescription = contentDescription,
-                    title = title)
-            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
+            ) {
 
+                Text(
+                    text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                                append("J")
+                        }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("C")
+                        }
+                        append("ompose")
+
+                    },
+                    fontSize = 30.sp,
+                    color = Color.White,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.LineThrough
+                )
+            }
 
         }
     }
 }
 
-@Composable
-fun ImageCard(painter: Painter,
-              contentDescription: String,
-              title: String,
-              modifier: Modifier = Modifier){
 
-    Card(modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-
-    ) {
-        Box(modifier = Modifier.height(200.dp)){
-
-           // val image : Painter = painterResource(id = R.drawable.kermit)
-
-            Image( painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            Box(modifier = Modifier
-                .fillMaxSize()
-                .background(Brush.verticalGradient(
-                    colors = listOf(
-                        Color.Transparent,
-                        Color.Black
-                    ), startY = 300f)
-                ))
-
-            Box(modifier = Modifier
-                .fillMaxHeight()
-                .padding(12.dp),
-                contentAlignment = Alignment.BottomStart){
-                Text(title, style = TextStyle(Color.White, fontSize = 16.sp))
-            }
-        }
-    }
-
-}
